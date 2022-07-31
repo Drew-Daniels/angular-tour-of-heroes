b4da727ecfc57335296148b234069fef92a49450
Drew Daniels <daniels_drew@protonmail.com>
2022-07-31 14:15:01 -0500

    Added a message service to notify and keep track of heroes selected.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from "../mock-heroes";
-import { HeroService } from "../hero.service";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,22 +11,23 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
 
+  selectedHero?: Hero;
+
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
 }
